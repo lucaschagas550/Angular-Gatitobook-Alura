0a013d435ff4c4edf240f6b5e9ef4ad9880e9439
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-11-19 15:18:57 -0300

    feat: organizando as rotas e lazyload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", pathMatch: 'full', redirectTo: 'home'},
+  {path: "home", loadChildren: ()=> import('./home/home.module').then((m) => m.HomeModule)}, //lazyload => carregamento sobre demanda
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
