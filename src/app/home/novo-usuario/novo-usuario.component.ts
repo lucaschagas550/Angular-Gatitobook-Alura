ab75cb9cf028a44dba65ab882e71ffbbc705431b
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-11-25 21:27:00 -0300

    feat: cadastro de usuario

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -5,6 +5,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
 import { minusculoValidator } from './minusculo.validor';
 import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais-validator';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -19,7 +20,8 @@ export class NovoUsuarioComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private novoUsuarioService: NovoUsuarioService,
-    private usuarioExisteService: UsuarioExisteService) { }
+    private usuarioExisteService: UsuarioExisteService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
@@ -39,9 +41,16 @@ export class NovoUsuarioComponent implements OnInit {
   }
 
   cadastrar() {
-    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
-    console.log("teste");
-    console.log(novoUsuario);
+    if (this.novoUsuarioForm.valid) {
+      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+      this.novoUsuarioService.cadastraNovoUsuario(novoUsuario).subscribe(() => {
+        this.router.navigate(['']);
+      },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
   }
 
 }
