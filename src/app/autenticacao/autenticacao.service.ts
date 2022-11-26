298eda8b83a64bbbc679e6aee31aa25610f49d1f
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-11-25 23:37:09 -0300

    feat: Token

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,14 +8,19 @@ import { Observable } from 'rxjs';
 export class AutenticacaoService {
 
   autenticacaoUrl = 'http://localhost:3000/usuarios';
+  tokenJWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.cThIIoDvwdueQB468K5xDc5633seEFoqwxjF_xSJyQQ';
+
   // 'http://localhost:3000/user/login'
   constructor(private httpClient: HttpClient) { }
 
-  autentica(usuario: string, senha: string) : Observable<any>{
+  autentica(usuario: string, senha: string): Observable<any> {
     // return this.httpClient.post(this.autenticacaoUrl,{
     //   username: usuario,
     //   password: senha
     // })
-    return this.httpClient.get(this.autenticacaoUrl + `?username=${usuario}&senha=${senha}`)
+    // var headers_object = new HttpHeaders().set("Authorization", "Bearer " + "teste");
+    var headers_object = new HttpHeaders().set("Authorization", "Bearer " + this.tokenJWT);
+
+    return this.httpClient.get(this.autenticacaoUrl + `?username=${usuario}&senha=${senha}`, { 'headers': headers_object })
   }
 }
