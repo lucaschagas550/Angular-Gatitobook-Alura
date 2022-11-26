8129de3782e92c173032ff0b044a9907869f65fa
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-11-26 20:53:08 -0300

    feat: servico de autenticacao

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,6 +1,7 @@
+import { UsuarioService } from './usuario/usuario.service';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,24 @@ export class AutenticacaoService {
   tokenJWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.cThIIoDvwdueQB468K5xDc5633seEFoqwxjF_xSJyQQ';
 
   // 'http://localhost:3000/user/login'
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient,
+    private usuarioService: UsuarioService) { }
 
-  autentica(usuario: string, senha: string): Observable<any> {
-    // return this.httpClient.post(this.autenticacaoUrl,{
-    //   username: usuario,
-    //   password: senha
-    // })
-    // var headers_object = new HttpHeaders().set("Authorization", "Bearer " + "teste");
+  autentica(usuario: string, senha: string): Observable<any> { //Observable<HttResponse<any>> //Para obter o conteudo todo do response{
+    return this.httpClient.post(this.autenticacaoUrl, {
+      username: usuario,
+      password: senha
+    },
+      {
+        observe: 'response'
+      }
+    ).pipe(
+      tap((response) => {
+        const authToken = response.headers.get('x-acess-token') ?? '';
+        this.usuarioService.salvaToken(authToken);
+      })
+    );
+
     var headers_object = new HttpHeaders().set("Authorization", "Bearer " + this.tokenJWT);
 
     return this.httpClient.get(this.autenticacaoUrl + `?username=${usuario}&senha=${senha}`, { 'headers': headers_object })
